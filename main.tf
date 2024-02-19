68bf1310ab7cc3823b21b9d385af6740d5df97c6
Craig Thacker <craigthackerx@gmail.com>
2024-02-19 21:31:47 +0000

    Update code

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
-#
+resource "azurerm_eventgrid_system_topic" "eventgrid_system_topic" {
+  for_each               = {for k, v in var.eventgrids : k => v}
+  name                   = each.value.name
+  resource_group_name    = each.value.rg_name
+  location               = each.value.location
+  tags                   = each.value.tags
+  source_arm_resource_id = each.value.source_arm_resource_id
+  topic_type             = each.value.topic_type
+
+  dynamic "identity" {
+    for_each = each.value.identity_type == "SystemAssigned" ? [each.value.identity_type] : []
+    content {
+      type = each.value.identity_type
+    }
+  }
+
+  dynamic "identity" {
+    for_each = each.value.identity_type == "UserAssigned" ? [each.value.identity_type] : []
+    content {
+      type         = each.value.identity_type
+      identity_ids = length(try(each.value.identity_ids, [])) > 0 ? each.value.identity_ids : []
+    }
+  }
+}
+
+
+
--- variables.tf
+++ variables.tf
@@ -1,19 +1,15 @@
-variable "location" {
-  description = "The location for this resource to be put in"
-  type        = string
-}
-
-variable "name" {
-  type        = string
-  description = "The name of the VNet gateway"
-}
-
-variable "rg_name" {
-  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
-  type        = string
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of the tags to use on the resources that are deployed with this module."
+variable "eventgrids" {
+  description = "The event grids blocks"
+  default     = null
+  type = list(object({
+    name                         = string
+    location                     = optional(string, "uksouth")
+    rg_name                      = string
+    create_event_subscription    = optional(bool, false)
+    tags                         = map(string)
+    source_arm_resource_id       = string
+    topic_type                   = string
+    identity_type                = optional(string)
+    identity_ids                 = optional(list(string))
+  }))
 }
